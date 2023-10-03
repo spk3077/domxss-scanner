a7f020e1644c8af6894fc098c7dfcafd0fa108c4
spk3077 <spk3077@rit.edu>
2023-10-02 20:42:46 -0400

    Safe Pipe added and main HTML edited

--- tests/spa/src/app/main/main.component.ts
+++ tests/spa/src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
+  @ViewChild('searchText') public filterResult!: ElementRef;
+  searchables: string[] = ["Patrick", "John", "Rose", "Thomas", "Jerry", "Chris", "Andrew"];
 
+  present!: boolean;
+  filterSearch!: string;
+
+
+  // Upon input in searchtext input should add content unsafely to DOM
+  public updateFilterText() {
+    if (this.filterSearch in this.searchables) {
+      this.present = true;
+    }
+    else {
+      this.present = false;
+    }
+    const domElement = this.filterResult.nativeElement;
+    const fragment = document.createRange().createContextualFragment(this.filterSearch);
+    domElement.appendChild(fragment);
+  }
 }
--- tests/spa/src/app/main/main.module.ts
+++ tests/spa/src/app/main/main.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { MainRoutingModule } from './main-routing.module';
+import { SafePipeModule } from './safe.pipe.module';
+
 import { MainComponent } from './main.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MainRoutingModule,
+    FormsModule,
+    SafePipeModule
   ],
   declarations: [MainComponent]
 })
