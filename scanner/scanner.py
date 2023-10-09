4dd876e47d3e39b6d4af1e2ef0521ade03a1ba24
spk3077 <spk3077@rit.edu>
2023-10-09 14:52:25 -0400

    Modified .gitignore, added cookie assessment, sadly test4 will be a bust due to selenium issues

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -9,6 +9,7 @@ Example Run: python3 scanner.py http://www.rit.edu/
 """
 import sys
 import time
+import re
 
 from vulnerability import Vulnerability
 from payloads import PAYLOADS
@@ -18,7 +19,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, UnexpectedAlertPresentException, NoAlertPresentException
+
 
 def check_input():
     """
@@ -58,6 +60,7 @@ def get_drivers() -> set:
     drivers.add(webdriver.Firefox(options = fire_options))
 
     # Edge
+    # Breaks when certain alerts are triggered. Cannot accept or dismiss certain alerts: aa'><img src=/ onerror=alert(1)> <!--
     edge_options = webdriver.EdgeOptions()
     edge_options.add_argument('--headless')
     drivers.add(webdriver.Edge(options = edge_options))
@@ -67,6 +70,7 @@ def get_drivers() -> set:
 
     return drivers
 
+
 def has_alert(driver) -> bool:
     """
     has_alert detects if an alert is present on the webpage
@@ -75,11 +79,111 @@ def has_alert(driver) -> bool:
     :return: boolean True if alert present, False if alert missing
     """
     try:
-        WebDriverWait(driver, 2.5).until(EC.alert_is_present())
+        WebDriverWait(driver, 3).until(EC.alert_is_present())
+        driver.switch_to.alert.dismiss()
+        driver.switch_to.default_content()
         return True
+    
+    # For Selenium Bug with Persistent Alert
+    except UnexpectedAlertPresentException:
+        pass
         
     except TimeoutException:
         return False
+    
+    try:
+        driver.switch_to.alert.accept()
+        return True
+    
+    except NoAlertPresentException:
+        return False
+
+
+def form_input_scan(driver, url: str) -> set:
+    """
+    form_input_scan scans the specified URL using the specified driver for INPUT/FORM DOM XSS
+
+    :param driver: browser object
+    :param url: Target URL to assess
+    :return: set of scan results
+    """
+    results: set = set()
+    # Detect inputs and manipulate them if detected
+    ## Inputs inside form
+    for payload in PAYLOADS:
+        for form in driver.find_elements(By.TAG_NAME, "form"):
+            # FUTURE WORK: SUPPORT MORE INPUT TYPES, FORM-RELATED TAGS
+            for input in form.find_elements(By.TAG_NAME, "input"):
+                if input.get_attribute("type") != "submit":
+                    input.send_keys(payload)
+    
+            form.submit()
+            if has_alert(driver):
+                results.add(Vulnerability("FORM", driver.current_url, payload, form))
+
+            # If we get redirected return to original site
+            driver.get(url)
+
+    ## Inputs outside form
+    for payload in PAYLOADS:
+        for input_element in driver.find_elements(By.TAG_NAME, "input"):
+            input_element.send_keys(payload)
+            
+            if has_alert(driver):
+                results.add(Vulnerability("INPUT", driver.current_url, payload, input_element))
+
+        # If we get redirected return to original site
+        driver.get(url)
+    
+    return results
+
+
+def query_scan(driver, url: str) -> set:
+    """
+    query_scan scans the specified URL using the specified driver for INPUT/FORM DOM XSS
+
+    :param driver: browser object
+    :param url: Target URL to assess
+    :return: set of scan results
+    """
+    results: set = set()
+    ## URL Parameter
+    #### window.location.search | location.search
+    exploit_url: str = ""
+    for payload in PAYLOADS:
+        if url.find("?") != -1: # ?
+            walked_url: str = url
+            while True:
+                equal_index: int = walked_url.find("=")
+                ampersand_index: int = walked_url.find("&")
+                fragment_index: int = walked_url.find("#")
+                if ampersand_index != -1:
+                    exploit_url = url[:equal_index + 1] + payload + url[ampersand_index:]
+                    walked_url = walked_url[ampersand_index + 1:]
+                elif fragment_index != -1:
+                    exploit_url = url[:equal_index + 1] + payload + url[fragment_index:]
+                    break
+                else:
+                    exploit_url = url[:equal_index + 1] + payload
+                    break
+
+                driver.get(exploit_url)
+                if has_alert():
+                    results.add(Vulnerability("QUERY", exploit_url, payload))
+
+        elif url.find("#") != -1: #
+            exploit_url = url[:url.find("#")] + "?exploit=" + payload + url[url.find("#"):]
+            driver.get(exploit_url)
+            if has_alert():
+                results.add(Vulnerability("QUERY", exploit_url, payload))
+
+        else: # Neither ? or #
+            exploit_url = url + "?exploit=" + payload
+            driver.get(exploit_url)
+            if has_alert(driver):
+                results.add(Vulnerability("QUERY", exploit_url, payload))
+
+    return results
 
 
 def scan_page(driver, url: str) -> set:
@@ -98,45 +202,43 @@ def scan_page(driver, url: str) -> set:
         print("Alert already present on assessed site")
         return
         
-    # Detect inputs and manipulate them if detected
-    ## Inputs inside form
-    found_inputs: set = set()
-    for form in driver.find_elements(By.TAG_NAME, "form"):
-        # FUTURE WORK: SUPPORT MORE INPUT TYPES, FORM-RELATED TAGS
-        for input in form.find_elements(By.TAG_NAME, "input"):
-            for payload in PAYLOADS:
-                input.send_keys(payload + Keys.RETURN)
-                found_inputs.add(input)
+    # Assess Manual Possibel Sources of DOM XSS
+    results.add(form_input_scan(driver, url))
+
+    # Assess Non-Manual Possible Sources of DOM XSS
+    results.add(query_scan(driver, url))
+
+    ## Cookie
+    ### Replace existing cookie values
+    driver.get(url)
+    for payload in PAYLOADS:
+        cookies_dict: dict = dict()
+        for cookie in driver.get_cookies():
+            cookies_dict['name'] = cookie['name']
+            cookies_dict['value'] = payload
         
-        form.submit()
-        if has_alert():
-            results.add(Vulnerability("FORM", form.current_url, form))
+        driver.add_cookie(cookies_dict)
+        driver.get(url)
 
-        # If we get redirected return to original site
-        if form.current_url != url:
-            driver.get(url)
+        if has_alert(driver):
+                results.add(Vulnerability("COOKIE", url, payload))
 
-    ## Inputs outside form
-    input_elements: list = form.find_elements(By.TAG_NAME, "input")
-    for input_element in input_elements:
-        if input_element not in found_inputs:
-            for payload in PAYLOADS:
-                input_element.send_keys(payload + Keys.RETURN)
-        
-                if has_alert():
-                    results.add(Vulnerability("INPUT", input_element.current_url, input_element))
-
-                # If we get redirected return to original site
-                if input_element.current_url != url:
-                    driver.get(url)
-
-
-    # Scan for Sources and manipulate them if detected
-
-
+    ### Add new cookie value
     
+    
+    # webdriver.Chrome().add_cookie(cookies_dict)
+    # driver.add_cookie({'name' : 'foo', 'value' : 'bar', 'path' : '/', 'secure' : True})
+
+
+
+    # ll_cookies=self.driver.get_cookies();
+    # cookies_dict = {}
+    # for cookie in all_cookies:
+    #     cookies_dict[cookie['name']] = cookie['value']
+    # print(cookies_dict)
 
     # execute_script
+    return results
 
 
 def print_output(results: set):
